1a48e63d7d6a96df31cf498de3388346f18cd6cc
mehul-raul <161235591+mehul-raul@users.noreply.github.com>
2024-05-09 09:28:18 +0530

    main.js

--- main.js
+++ main.js
@@ -58,8 +58,8 @@ function randomint (min,max){
     return Math.floor(Math.random()*(max-min)+min)
 }
 
-function indicatorcol(color){
-   strengthcolor.style.backgroundColor=color;
+function indicatorcolor(color){
+   strengthcolor.style.backgroundcolor=(color);
    strengthcolor.style.boxShadow = `0px 0px 12px 1px ${color}`;
 
 }
@@ -188,6 +188,6 @@ generatebutt.addEventListener("click", () => {
     password = passwordArray.join('');
 
     passdisp.value = password;
-    stren();
+    
 
 })
